1e8691b1c42144a33f3c86706b6c1ac69231bad7
Eldar <esliceyament@gmail.com>
2024-11-17 14:52:24 +0400

    bugs fixed

---
+++ Product-Service/src/main/java/com/example/productservice/exception/DiscountPriceException.java
@@ -0,0 +1,7 @@
+package com.example.productservice.exception;
+
+public class DiscountPriceException extends RuntimeException {
+    public DiscountPriceException(String message) {
+        super(message);
+    }
+}
